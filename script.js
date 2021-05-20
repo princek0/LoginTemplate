f2dc433f21555513283ce25191934de5fc5c2aed
Prince <62904535+princek0@users.noreply.github.com>
2021-05-20 15:34:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function realtimeClock() {
+
+  var rtClock = new Date();
+  var hours = rtClock.getHours();
+  var minutes = rtClock.getMinutes();
+  var seconds = rtClock.getSeconds();
+  var amPm = (hours < 12) ? "AM" : "PM";
+
+  hours = (hours > 12) ? hours - 12 : hours;
+  hours = ("0" + hours).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+  seconds = ("0" + seconds).slice(-2);
+
+  document.getElementById('clock').innerHTML = hours + "  :  " + minutes + "  :  " + seconds +  " ";
+  var t = setTimeout(realtimeClock, 500);
+
+}
+
+function validate() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if ( username == "Test" && password == "Test"){
+    alert ("Successfull login.")
+    window.location = "success.html";
+    return false;
+  }
+  else{
+    alert ("Unsuccessful login.")
+    return false;
+  }
+}
